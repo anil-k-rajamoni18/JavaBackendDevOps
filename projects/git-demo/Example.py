cda350db39af9e72c9f4b12b9c55ffd18d5d9331
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2025-03-01 13:48:07 +0530

    added new changes in py java yml file

--- projects/git-demo/Example.py
+++ projects/git-demo/Example.py
@@ -1,2 +1,6 @@
 ## Welcome print
 print("Welcome to Py..")
+
+num1 = int(input("Enter Num1: "))
+num2 = int(input("Enter Num2: "))
+print("Addition: ", (num1 + num2))
--- projects/git-demo/Sample.java
+++ projects/git-demo/Sample.java
@@ -1,5 +1,13 @@
+interface Greet {
+    String greetUser(final String msg);
+}
+
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Welcome to Java 8 Features...");
+        
+        // Lambda 
+        Greet greet = (msg) -> "Hello, " + msg;
+        System.out.println(greet.greetUser("Kumar"));
     }
 }
