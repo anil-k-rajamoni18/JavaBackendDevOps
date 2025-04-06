f6ae65a8a55ac9ee5b6250c4e75485bdf9236984
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2025-04-06 07:50:26 +0530

    update

--- projects/nodejs-crud-app/frontend/vite.config.js
+++ projects/nodejs-crud-app/frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['ec2-43-204-236-117.ap-south-1.compute.amazonaws.com']
   }
 })
